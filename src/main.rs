ecf2db64adbe2fb6073b80a362073fef84f7e32f
Clyne Fuentes <clippefuentes@gmail.com>
2022-09-14 20:40:13 -0600

    add text

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@
 use rusty_engine::prelude::*;
 
 struct GameState {
-    // high_score: u32,
-    current_score: u32,
+    high_score: u32,
+    score: u32,
     // enemy_labels: Vec<String>,
     ferris_index: i32,
     // spawn_timer: Timer,
@@ -13,10 +13,10 @@ struct GameState {
 impl Default for GameState {
     fn default() -> Self {
         Self {
-            // high_score: 0,
-            current_score: 0,
+            high_score: 0,
+            score: 0,
             ferris_index: 0,
-            // spawn_timer: Timer::from_seconds(1.0, false), 
+            // spawn_timer: Timer::from_seconds(1.0, false),
         }
     }
 }
@@ -30,6 +30,11 @@ fn main() {
     player.rotation = SOUTH_WEST;
     player.scale = 1.0;
     player.collision = true;
+
+    let score = game.add_text("score", "Score: 0");
+    score.translation = Vec2::new(520.0, 321.0);
+    let high_score = game.add_text("high_score", "High Score: 0");
+    high_score.translation = Vec2::new(-520.0, 321.0);
 
     game.add_logic(game_logic);
     game.run(GameState::default());
@@ -46,6 +51,15 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
                     engine.sprites.remove(&label);
                 }
             }
+            game_state.score += 1;
+            let score = engine.texts.get_mut("score").unwrap();
+            score.value = format!("Score: {}", game_state.score);
+
+            if game_state.score > game_state.high_score {
+                game_state.high_score = game_state.score;
+                let high_score = engine.texts.get_mut("high_score").unwrap();
+                high_score.value = format!("High score: {}", game_state.high_score);
+            }
         }
     }
 
@@ -54,25 +68,25 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
     const MOVEMENT_SPEED: f32 = 100.0;
     if engine
         .keyboard_state
-        .pressed_any(&[KeyCode::Up, KeyCode::W]) 
+        .pressed_any(&[KeyCode::Up, KeyCode::W])
     {
         player.translation.y += MOVEMENT_SPEED * engine.delta_f32;
     }
     if engine
         .keyboard_state
-        .pressed_any(&[KeyCode::Down, KeyCode::S]) 
+        .pressed_any(&[KeyCode::Down, KeyCode::S])
     {
         player.translation.y -= MOVEMENT_SPEED * engine.delta_f32;
     }
     if engine
         .keyboard_state
-        .pressed_any(&[KeyCode::Right, KeyCode::D]) 
+        .pressed_any(&[KeyCode::Right, KeyCode::D])
     {
         player.translation.x += MOVEMENT_SPEED * engine.delta_f32;
     }
     if engine
         .keyboard_state
-        .pressed_any(&[KeyCode::Left, KeyCode::A]) 
+        .pressed_any(&[KeyCode::Left, KeyCode::A])
     {
         player.translation.x -= MOVEMENT_SPEED * engine.delta_f32;
     }
@@ -87,5 +101,11 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
             car1.translation = mouse_location;
             car1.collision = true;
         }
+    }
+
+    if engine.keyboard_state.just_pressed(KeyCode::R) {
+        game_state.score = 0;
+        let score = engine.texts.get_mut("score").unwrap();
+        score.value = "Score: 0".to_string();
     }
 }
