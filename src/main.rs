4c183153c976568c82f9876c876fba3f3b4d8b31
Clyne Fuentes <clippefuentes@gmail.com>
2022-09-15 18:11:41 -0600

    add audio

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,8 @@ impl Default for GameState {
 fn main() {
     let mut game = Game::new();
 
+    game.audio_manager.play_music(MusicPreset::WhimsicalPopsicle, 0.1);
+
     let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
     player.translation = Vec2::new(0.0, 0.0);
     // player.rotation = std::f32::consts::FRAC_PI_2;
@@ -60,6 +62,7 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
                 let high_score = engine.texts.get_mut("high_score").unwrap();
                 high_score.value = format!("High score: {}", game_state.high_score);
             }
+            engine.audio_manager.play_sfx(SfxPreset::Minimize1, 0.1);
         }
     }
 
