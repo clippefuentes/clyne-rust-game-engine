89e792a21e95d9b288f96cd447042062d7587b46
Clyne Fuentes <clippefuentes@gmail.com>
2022-09-17 12:45:54 -0600

    add road and obstacle

--- src/main.rs
+++ src/main.rs
@@ -10,16 +10,20 @@ struct GameState {
     score: u32,
     // enemy_labels: Vec<String>,
     ferris_index: i32,
-    spawn_timer: Timer,
+    // spawn_timer: Timer,
+    health_amount: u8,
+    lost: bool,
 }
 
 impl Default for GameState {
     fn default() -> Self {
         Self {
+            health_amount: 5,
+            lost: false,
             high_score: 0,
             score: 0,
             ferris_index: 0,
-            spawn_timer: Timer::from_seconds(2.0, true),
+            // spawn_timer: Timer::from_seconds(2.0, true),
         }
     }
 }
@@ -34,10 +38,11 @@ fn main() {
     game.audio_manager.play_music(MusicPreset::WhimsicalPopsicle, 0.1);
 
     let player = game.add_sprite("player", SpritePreset::RacingCarBlue);
-    player.translation = Vec2::new(0.0, 0.0);
+    player.translation = Vec2::new(-500.0, 0.0);
+    player.layer = 10.0;
     // player.rotation = std::f32::consts::FRAC_PI_2;
-    player.rotation = SOUTH_WEST;
-    player.scale = 1.0;
+    // player.rotation = SOUTH_WEST;
+    // player.scale = 1.0;
     player.collision = true;
 
     let score = game.add_text("score", "Score: 0");
@@ -45,9 +50,31 @@ fn main() {
     let high_score = game.add_text("high_score", "High Score: 0");
     high_score.translation = Vec2::new(-520.0, 321.0);
 
+    for i in 0..10 {
+        let roadline = game.add_sprite(format!("roadline{}", i), SpritePreset::RacingBarrierWhite);
+        roadline.scale = 0.1;
+        roadline.translation.x = -600.0 + 150.0 * i as f32;
+    }
+
+    let obstable_present = vec![
+        SpritePreset::RacingBarrelBlue,
+        SpritePreset::RacingBarrelRed,
+        SpritePreset::RacingConeStraight,
+    ];
+
+    for (i, preset) in obstable_present.into_iter().enumerate() {
+        let obstable = game.add_sprite(format!("obstacle{}", i), preset);
+        obstable.layer = 5.0;
+        obstable.collision = true;
+        obstable.translation.x = thread_rng().gen_range(800.0..1600.0);
+        obstable.translation.y = thread_rng().gen_range(-300.0..300.0);
+    }
     game.add_logic(game_logic);
     game.run(GameState::default());
 }
+
+const MOVEMENT_SPEED: f32 = 350.0;
+const ROAD_SPEED: f32 = 400.0;
 
 fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
     // Quit if Q is presed
@@ -91,18 +118,23 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
 
     let player = engine.sprites.get_mut("player").unwrap();
     // player.translation.x += 100.0 * engine.delta_f32;
-    const MOVEMENT_SPEED: f32 = 350.0;
+    player.rotation = EAST;
     if engine
         .keyboard_state
         .pressed_any(&[KeyCode::Up, KeyCode::W])
     {
+        if player.translation.y < -360.0 || player.translation.y > 360.0 {
+            game_state.health_amount = 0;
+        }
         player.translation.y += MOVEMENT_SPEED * engine.delta_f32;
+        player.rotation = (1.0) * 0.15;
     }
     if engine
         .keyboard_state
         .pressed_any(&[KeyCode::Down, KeyCode::S])
     {
         player.translation.y -= MOVEMENT_SPEED * engine.delta_f32;
+        player.rotation = -(1.0) * 0.15;
     }
     if engine
         .keyboard_state
@@ -129,18 +161,34 @@ fn game_logic(engine: &mut Engine, game_state: &mut GameState) {
         }
     }
 
-    if game_state.spawn_timer.tick(engine.delta).just_finished() {
-        let label = format!("ferris{}", game_state.ferris_index);
-        game_state.ferris_index += 1;
-        let car1 = engine.add_sprite(label.clone(), SpritePreset::RacingCarYellow);
-        car1.translation.x = thread_rng().gen_range(-550.0..550.0);
-        car1.translation.y = thread_rng().gen_range(-325.0..325.0);
-        car1.collision = true;
-    }
+    // if game_state.spawn_timer.tick(engine.delta).just_finished() {
+    //     let label = format!("ferris{}", game_state.ferris_index);
+    //     game_state.ferris_index += 1;
+    //     let car1 = engine.add_sprite(label.clone(), SpritePreset::RacingCarYellow);
+    //     car1.translation.x = thread_rng().gen_range(-550.0..550.0);
+    //     car1.translation.y = thread_rng().gen_range(-325.0..325.0);
+    //     car1.collision = true;
+    // }
 
     if engine.keyboard_state.just_pressed(KeyCode::R) {
         game_state.score = 0;
         let score = engine.texts.get_mut("score").unwrap();
         score.value = "Score: 0".to_string();
+    }
+
+    for sprite in engine.sprites.values_mut() {
+        if sprite.label.starts_with("roadline") {
+            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
+            if sprite.translation.x < -675.0 {
+                sprite.translation.x += 1500.0;
+            }
+        }
+        if sprite.label.starts_with("obstacle") {
+            sprite.translation.x -= ROAD_SPEED * engine.delta_f32;
+            if sprite.translation.x < -800.0 {
+                sprite.translation.x = thread_rng().gen_range(800.0..1600.0);
+                sprite.translation.y = thread_rng().gen_range(-300.0..300.0);
+            }
+        }
     }
 }
